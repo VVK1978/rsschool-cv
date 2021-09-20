01f15f9c4180db0e5ae16e1a1e4de20dc0b6a076
VVK1978 <70840645+VVK1978@users.noreply.github.com>
2021-09-20 09:20:52 +0300

    feat: add my code from codewars wiht highlightjs and part my projects, add images of my projects

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const closeBtn = document.getElementById('close__btn');
 const itemBurger = document.querySelectorAll('.navigation__menu-burger');
-itemBurger.forEach(element => element.addEventListener('click', (e) => {
+itemBurger.forEach((element) =>
+  element.addEventListener('click', (e) => {
     closeBtn.click();
-}));
+  })
+);
+const projectImageSrc = ['./img/oz.png', './img/pf.png', './img/vp.png', './img/efk.png'];
+const projectLinks = [
+  'https://rolling-scopes-school.github.io/vvk1978-JSFE2021Q1/online-zoo/pages/main/',
+  'https://rolling-scopes-school.github.io/vvk1978-JSFE2021Q1/photo-filter/',
+  'https://rolling-scopes-school.github.io/vvk1978-JSFE2021Q1/virtual-piano/',
+  'https://rolling-scopes-school.github.io/vvk1978-JSFE2021Q1/english-for-kids-base/build/',
+];
+const projectLink = document.querySelector('.project-link');
+const projectImage = document.querySelector('.slider-image');
+const slider = document.querySelector('.slider');
+const dots = document.querySelectorAll('.slider-dot');
+let dotActive = 0;
+dots.forEach((element) =>
+  element.addEventListener('click', (event) => {
+    dots[dotActive].classList.remove('active');
+    dotActive = +event.target.dataset.dot;
+    dots[dotActive].classList.add('active');
+    projectImage.setAttribute('src', projectImageSrc[dotActive]);
+    projectLink.setAttribute('href', projectLinks[dotActive]);
+  })
+);
